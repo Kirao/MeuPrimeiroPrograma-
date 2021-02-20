158b9935f8a2daf42202ceac91501950d7babbf8
Guilherme Henrique Bodo <guihenrique1231@hotmail.com>
2021-02-20 00:06:47 -0300

    Add files via Upload
    Adicionei o primeiro contato com a parte de programação em c# que fiz :3

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿//biblioteca para execução de recursos básicos do computador
+using System;
+
+				
+//Declaração de uma CLASSE pública com o nome de Program
+public class Program
+{
+	//Declaração de um método ESTÁTICO com o nome main
+	public static void Main()
+	{
+		Console.WriteLine("Boa Noite");
+	    Console.WriteLine("Meu nome é:Guilherme");
+		Console.WriteLine("E tenho 20 Anos");
+		Console.Write("oie ");
+		Console.Write("td bem?");
+					
+	}
+}
